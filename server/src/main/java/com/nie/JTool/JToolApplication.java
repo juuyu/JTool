339ee78ca5784ba701e11a23850a53d089266232
nie <junyunie@outlook.com>
2024-06-24 13:52:17 +0800

    first commit

--- server/src/main/java/com/nie/JTool/JToolApplication.java
+++ server/src/main/java/com/nie/JTool/JToolApplication.java
@@ -3,10 +3,13 @@ package com.nie.JTool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class JToolApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
 		SpringApplication.run(JToolApplication.class, args);
 	}
 
